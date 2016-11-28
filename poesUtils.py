d5e5f855b779a2979888b41ac4aa02e971e485e3
bharatreddy <bharatr@vt.edu>
2016-11-28 14:30:44 -0500

    download and semi process poes data

--- poesUtils.py
+++ poesUtils.py
@@ -1,10 +1,17 @@
 if __name__ == "__main__":
     import datetime
     import poesUtils
-    inpDate = datetime.date(2016,1,2)
+    inpDate = datetime.date(2015,6,17)
     poesObj = poesUtils.PoesData(inpDate)
-    poesFiles = poesObj.get_all_sat_data(outDir="/Users/bharat/Desktop/poesTest")
-    print poesFiles
+    # poesFiles = poesObj.get_all_sat_data(outDir="/home/bharat/Desktop/poesTest")
+    testFiles = ['/home/bharat/Desktop/poesTest/poes_n18_20150617_proc.nc', \
+                '/home/bharat/Desktop/poesTest/poes_m01_20150617_proc.nc', \
+                '/home/bharat/Desktop/poesTest/poes_n19_20150617_proc.nc', \
+                '/home/bharat/Desktop/poesTest/poes_m02_20150617_proc.nc', 
+                '/home/bharat/Desktop/poesTest/poes_n15_20150617_proc.nc']
+    poesObj.read_poes_data_files(testFiles, \
+                     eleFluxFile="/home/bharat/Desktop/poesTest/eleflux.csv",\
+                     proFluxFile="/home/bharat/Desktop/poesTest/proflux.csv")
 
 class PoesData(object):
     """
@@ -41,6 +48,8 @@ class PoesData(object):
         import bs4
         import shutil
         import os
+        # generate urls to download POES satellite data from 
+        # all files for a given date
         urlDict = self.get_all_sat_urls()
         if urlDict is None:
             print "url retreival failed!"
@@ -74,4 +83,82 @@ class PoesData(object):
 
     def get_file_from_url(self, url, fileName):
         import urllib
+        # Download a given poes file
         urllib.urlretrieve(url + fileName, fileName)
+
+    def read_poes_data_files(self, fileList, eleFluxFile="./eleflux.csv",\
+                     proFluxFile="./proflux.csv"):
+        import netCDF4
+        import pandas
+        import datetime
+        import numpy
+        # read data from given POES files
+        # Note these should be local files
+        # and I expect a list for input
+        if not isinstance(fileList, list):
+            print "input fileList should be a list type"
+            return None
+        if len(fileList) == 0:
+            print "fileList input is empty"
+            return None
+        # We'll store all the data into two dataframes
+        # one for electron flux and the other for protons
+        poesAllEleDataDF = pandas.DataFrame( columns =  ["timestamp", "date", "aacgm_lat_foot",\
+                         "aacgm_lon_foot", "MLT", "log_ele_flux", "sat"] )
+        poesAllProDataDF = pandas.DataFrame( columns =  ["timestamp", "date", "aacgm_lat_foot",\
+                                 "aacgm_lon_foot", "MLT", "log_pro_flux", "sat"] )
+        for f in fileList:
+            print "reading file-->", f
+            # read variable from the netCDF files
+            poesRawData = netCDF4.Dataset(f)
+            poesDF = pandas.DataFrame( poesRawData.variables['time'][:], columns=[ "timestamp" ] )
+            poesDF['date'] = pandas.to_datetime(poesDF['timestamp'], unit='ms')
+            poesDF["alt"] = poesRawData.variables['alt'][:]
+            poesDF["aacgm_lat_foot"] = poesRawData.variables['aacgm_lat_foot'][:]
+            poesDF["aacgm_lon_foot"] = poesRawData.variables['aacgm_lon_foot'][:]
+            poesDF["MLT"] = poesRawData.variables['MLT'][:]
+            # Add up the fluxes
+            poesDF["ted_ele_total_flux"] = poesRawData.variables['ted_ele_tel0_flux_4'][:] +\
+                    poesRawData.variables['ted_ele_tel0_flux_8'][:] + \
+                    poesRawData.variables['ted_ele_tel0_flux_11'][:] + \
+                    poesRawData.variables['ted_ele_tel0_flux_14'][:] + \
+                    poesRawData.variables['ted_ele_tel30_flux_4'][:] +\
+                    poesRawData.variables['ted_ele_tel30_flux_8'][:] + \
+                    poesRawData.variables['ted_ele_tel30_flux_11'][:] + \
+                    poesRawData.variables['ted_ele_tel30_flux_14'][:]
+            poesDF["ted_pro_total_flux"] = poesRawData.variables['ted_pro_tel0_flux_4'][:] +\
+                    poesRawData.variables['ted_pro_tel0_flux_8'][:] + \
+                    poesRawData.variables['ted_pro_tel0_flux_11'][:] + \
+                    poesRawData.variables['ted_pro_tel0_flux_14'][:] + \
+                    poesRawData.variables['ted_pro_tel30_flux_4'][:] +\
+                    poesRawData.variables['ted_pro_tel30_flux_8'][:] + \
+                    poesRawData.variables['ted_pro_tel30_flux_11'][:] + \
+                    poesRawData.variables['ted_pro_tel30_flux_14'][:]
+            poesDF['log_ele_flux'] = [0. if x <= 0. else numpy.log10(x)\
+                         for x in poesDF['ted_ele_total_flux']]
+            poesDF['log_pro_flux'] = [0. if x <= 0. else numpy.log10(x)\
+                         for x in poesDF['ted_pro_total_flux']]
+            # the current satellite number
+            poesDF["sat"] = f[-20:-17]
+            # seperate out electron and proton flux and discard all zeros
+            currPoesEleFluxDF = poesDF[poesDF["log_ele_flux"] > 0.][ ["timestamp",\
+                             "date", "aacgm_lat_foot", "aacgm_lon_foot", "MLT",\
+                             "log_ele_flux", "sat"] ].reset_index(drop=True)
+            currPoesProFluxDF = poesDF[poesDF["log_pro_flux"] > 0.][ ["timestamp",\
+                             "date", "aacgm_lat_foot", "aacgm_lon_foot", "MLT",\
+                             "log_pro_flux", "sat"] ].reset_index(drop=True)
+            poesAllEleDataDF = poesAllEleDataDF.append( currPoesEleFluxDF )
+            poesAllProDataDF = poesAllProDataDF.append( currPoesProFluxDF )
+            # now delete all the rows for prev DFs
+            # we don't want to duplicate data
+            poesDF = poesDF.drop( poesDF.index )
+            currPoesEleFluxDF = currPoesEleFluxDF.drop( currPoesEleFluxDF.index )
+            currPoesProFluxDF = currPoesProFluxDF.drop( currPoesProFluxDF.index )
+        # create a date and time columns
+        poesAllEleDataDF["dateStr"] = poesAllEleDataDF["date"].map(lambda x: x.strftime('%Y%m%d'))
+        poesAllEleDataDF["time"] = poesAllEleDataDF["date"].map(lambda x: x.strftime('%H%M'))
+        poesAllProDataDF["dateStr"] = poesAllProDataDF["date"].map(lambda x: x.strftime('%Y%m%d'))
+        poesAllProDataDF["time"] = poesAllProDataDF["date"].map(lambda x: x.strftime('%H%M'))
+        # save as csv files
+        poesAllEleDataDF.to_csv(eleFluxFile, sep=' ', index=False)
+        poesAllProDataDF.to_csv(proFluxFile, sep=' ', index=False)
