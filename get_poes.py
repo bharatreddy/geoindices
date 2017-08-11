7ce789854598cfca6bad8442469ae5548936d4f2
bharatreddy <bharatr@vt.edu>
2017-08-11 18:55:48 -0400

    bnds vals

--- get_poes.py
+++ get_poes.py
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     timeRange = [ poesAllEleDataDF["date"].min(), poesAllEleDataDF["date"].max() ]
     aurPassDF = poesObj.get_closest_sat_passes( poesAllEleDataDF, poesAllProDataDF, timeRange )
     eqBndLocsDF = poesObj.get_nth_ele_eq_bnd_locs( aurPassDF, poesAllEleDataDF )
+    print eqBndLocsDF
