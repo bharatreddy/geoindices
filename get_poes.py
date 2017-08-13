60a6d4b255e913da86077293fbd8453c9693039c
bharatreddy <bharatr@vt.edu>
2017-08-12 20:57:12 -0400

    loop through dates and create files

--- get_poes.py
+++ get_poes.py
@@ -1,20 +1,28 @@
 if __name__ == "__main__":
     import datetime
     import poesUtils
-    inpDate = datetime.date(2013,3,2)
-    poesObj = poesUtils.PoesData(inpDate)
-    # poesFiles = poesObj.get_all_sat_data(outDir=".")
-    poesFiles = [ "./poes_n15_20130302_proc.nc",\
-                "./poes_n18_20130302_proc.nc",\
-                "./poes_m01_20130302_proc.nc",\
-                "./poes_n19_20130302_proc.nc",\
-                "./poes_m02_20130302_proc.nc",\
-                "./poes_n16_20130302_proc.nc",\
-                "./poes_n17_20130302_proc.nc" ]
-    poesAllEleDataDF, poesAllProDataDF = poesObj.read_poes_data_files( poesFiles )
-    # print type(poesAllEleDataDF["date"].min())
-    timeRange = [ poesAllEleDataDF["date"].min(), poesAllEleDataDF["date"].max() ]
-    aurPassDF = poesObj.get_closest_sat_passes( poesAllEleDataDF, poesAllProDataDF, timeRange )
-    eqBndLocsDF = poesObj.get_nth_ele_eq_bnd_locs( aurPassDF, poesAllEleDataDF )
-    fitBndDF = poesObj.fit_circle_aurbnd( eqBndLocsDF )
-    print fitBndDF
+    import os
+    sTimePOES = datetime.datetime( 2014,1,1 )
+    eTimePOES = datetime.datetime( 2015,12,31 )
+    dayCount = (eTimePOES - sTimePOES).days + 1
+    for inpDate in (sTimePOES + \
+            datetime.timedelta(n) for n in range(dayCount)):
+        print "currently working with---->", inpDate
+        poesObj = poesUtils.PoesData(inpDate)
+        change outDir bro! to shift them to a different loc
+        poesFiles = poesObj.get_all_sat_data(outDir=".")
+        # poesFiles = [ "./poes_n15_20130302_proc.nc",\
+        #             "./poes_n18_20130302_proc.nc",\
+        #             "./poes_m01_20130302_proc.nc",\
+        #             "./poes_n19_20130302_proc.nc",\
+        #             "./poes_m02_20130302_proc.nc",\
+        #             "./poes_n16_20130302_proc.nc",\
+        #             "./poes_n17_20130302_proc.nc" ]
+        poesAllEleDataDF, poesAllProDataDF = poesObj.read_poes_data_files( poesFiles )
+        timeRange = [ poesAllEleDataDF["date"].min(), poesAllEleDataDF["date"].max() ]
+        aurPassDF = poesObj.get_closest_sat_passes( poesAllEleDataDF, poesAllProDataDF, timeRange )
+        eqBndLocsDF = poesObj.get_nth_ele_eq_bnd_locs( aurPassDF, poesAllEleDataDF )
+        poesObj.fit_circle_aurbnd( eqBndLocsDF )
+        # remove the POES files
+        for pFile in poesFiles:
+            os.remove(pFile)
